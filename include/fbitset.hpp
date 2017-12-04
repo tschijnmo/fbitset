5e504b9fd48d636af2bac72283b31d545bff1d28
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-12-04 14:32:57 -0600

    Layout the basic scaffold of the project
    The single header file and the main test driver are all added and
    configured by the CMake builder.

---
+++ include/fbitset.hpp
@@ -0,0 +1,11 @@
+/** The main header file for fbitset.
+ *
+ * In order to use the library, including this header file is sufficient.
+ *
+ */
+
+
+#ifndef FBITSET_FBITSET_H
+#define FBITSET_FBITSET_H
+
+#endif
