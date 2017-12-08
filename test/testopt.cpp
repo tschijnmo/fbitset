bf82ac2eb103baef402f346bdd153d78d66a214b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-12-08 17:29:11 -0600

    Add optimization test for hashing
    Currently we concentrate on the hashing of a single 64-bit limb.

--- test/testopt.cpp
+++ test/testopt.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <cstdint>
+#include <functional>
 #include <vector>
 
 #include <fbitset.hpp>
@@ -52,3 +53,15 @@ Two64_no_ext union_two64_no_ext(Two64_no_ext& o1, Two64_no_ext& o2)
 // of two 64-bit integers.
 
 One64_ext union_one64_ext(One64_ext& o1, One64_ext& o2) { return o1 | o2; }
+
+//
+// Hashing
+//
+
+// Hashing a single 64-bit should be basically optimized to nothing.
+
+size_t hash_one64_no_ext(One64_no_ext& inp)
+{
+    std::hash<One64_no_ext> hash{};
+    return hash(inp);
+}
